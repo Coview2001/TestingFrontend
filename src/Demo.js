faaeb0dc764c7bd0837024ed48b18e9220c5ada6
Pavan Kalyan <PAV2BLR@TPS.COM>
2025-01-16 10:58:42 +0530

    16-01-25

---
+++ src/Demo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+ 
+function Demo() {
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        fetchData();
+    }, []);
+   
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://surgebackend.azurewebsites.net//');
+        const data = response.data;
+ 
+        setCourses(data.Message);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  return (
+    <div className='container-fluid'><div className='text-center mt-5 fw-bold border border-success p-2 rounded '  style={{color:"green",fontSize:"16px"}} > {courses}</div>
+      <p className='text-end fw-italic pe-5' style={{fontSize:"18px"}}>- From Pavan </p>
+    </div>
+  )
+}
+ 
+export default Demo
